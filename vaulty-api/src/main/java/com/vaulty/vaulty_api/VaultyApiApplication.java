7fb01d68a123efc2c562518b81295459d4adb250
INDRA\cmunoza <cmunoza@minsait.com>
2026-09-16 16:03:09 +0200

    Proyecto base creado

---
+++ vaulty-api/src/main/java/com/vaulty/vaulty_api/VaultyApiApplication.java
@@ -0,0 +1,13 @@
+package com.vaulty.vaulty_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultyApiApplication.class, args);
+	}
+
+}
---
+++ vaulty-api/src/test/java/com/vaulty/vaulty_api/VaultyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaulty.vaulty_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
